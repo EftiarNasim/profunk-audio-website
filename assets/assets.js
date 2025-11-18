47b703976e8b22600ff1a53abf408b19f2c354f3
EftiarNasim <eftiar.nasim@gmail.com>
2025-11-18 12:37:20 +0400

    first commit

---
+++ assets/assets.js
@@ -0,0 +1,13 @@
+document.getElementById("title").innerHTML="Sound Stories";
+document.getElementById("subtitle").innerHTML="Over 160,000 stunning sound effects (SFX) and over 650 royalty free music tracks free for commercial and non commercial use.";
+function showMessage() {
+    alert("Button clicked!");
+}
+function showMessageExplor(){
+    alert("Successfuly Explored")
+}
+document.getElementById("btn").addEventListener("click",showMessageExplor);
+document.getElementById("effect-title").innerHTML="Latest Sound Effects";
+
+document.getElementById("popular-heading").innerHTML="Popular Sound Effect";
+document.getElementById("popular-description").innerHTML="Unique sound effects for download. You won’t find them anywhere else.";
